1ba558df4c8e1d6b1d093f1cd39cfd8abc34ee42
Warn <imwarn@163.com>
2021-02-20 14:47:51 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,6 @@
 var usemail = '398949820@qq.com';
 var usqq = '398949820';
-var usphone = '010-58326589';
+var usphone = '18612149479';
 var usaddress = '北京市朝阳区高碑店乡高碑店村民俗文化街1376号华膳园文化传媒产业园12号楼二层227室';
 var beian = '京ICP备2021000501号-2';
 var uscompany = '北京牛角科技有限公司';
@@ -36,7 +36,7 @@ function createFooter(){
     var currentYear = new Date().getFullYear();
     var _foot = '<p>联系邮箱：'+ usemail +'&nbsp;&nbsp;联系QQ：'+ usqq +'&nbsp;&nbsp;联系电话：'+ usphone +'</p>'+
         '<p>联系地址：'+ usaddress +'&nbsp;&nbsp;©'+currentYear+'&nbsp;&nbsp;'+ uscompany +' 版权所有 </p>'+
-        '<p><a href="https://beian.miit.gov.cn/" target="_blank">'+ beian +'</a>&nbsp;&nbsp;<a href="/static/images/papers/business_license_2019.jpg" target="_blank">网络文化经营许可证 京 网文（2019）1348-140号</a></p>';
+        '<p><a href="https://beian.miit.gov.cn/" target="_blank">'+ beian +'</a>&nbsp;&nbsp;<a href="/static/images/papers/business_license.jpg" target="_blank">网络文化经营许可证 京网文（2021）0372-110号</a></p>';
     $('.footer .bottom').append(_foot);
 }
 
